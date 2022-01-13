3698e380841a21a465d3d2777e11ba9acad1e413
Will Russell <will@wrussell.co.uk>
2022-01-13 18:07:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# MLH Prep Project
+
+Over the next 2 weeks, you'll be building a React App that works with various APIs (Application Programming Interfaces) that talk to different data sources to do cool stuff.
+
+We're using the [OpenWeather API](https://openweathermap.org/current) to get weather data on different cities. Your challenge over the next 2 weeks is to build out this website and add even more functionality to it. At the moment, it displays basic information about a location when you type it in. Check out [Issues](/issues) for some more ideas!
+
+You'll need to get your own API Key from their website (for free) and add it as an environment variable in a `.env` file. We have a template available as `example.env`.
+
+You'll be using React initially to build this. If you're new to React, check out the [website](https://reactjs.org) for some information on getting started! 
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import logo from './mlh-prep.png'
+
+function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [city, setCity] = useState("New York City")
+  const [results, setResults] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          if (result['cod'] !== 200) {
+            setIsLoaded(false)
+          } else {
+            setIsLoaded(true);
+            setResults(result);
+          }
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [city])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else {
+    return <>
+      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+      <div>
+        <h2>Enter a city below 👇</h2>
+        <input
+          type="text"
+          value={city}
+          onChange={event => setCity(event.target.value)} />
+        <div className="Results">
+          {!isLoaded && <h2>Loading...</h2>}
+          {console.log(results)}
+          {isLoaded && results && <>
+            <h3>{results.weather[0].main}</h3>
+            <p>Feels like {results.main.feels_like}°C</p>
+            <i><p>{results.name}, {results.sys.country}</p></i>
+          </>}
+        </div>
+      </div>
+    </>
+  }
+}
+
+export default App;
