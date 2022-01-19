c7a0b3fc0a2e099d572a8215083b54302bca2e9e
Aniket762 <aniketindian8@gmail.com>
2022-01-19 16:23:30 +0530

    Feat: 4 hours weather added

--- src/components/hourlyWeather.js
+++ src/components/hourlyWeather.js
@@ -14,8 +14,32 @@ const HourlyWeather = ({ city }) => {
 
         async function fetchWeatherInfo(lat, lon){
             const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_APIKEY}&units=metric`).then(value=>value.json());
+            console.log(response["hourly"])
             return response["hourly"];
         }
+
+        async function fetchHourlyWeather(city) {
+            const [latitude, longitude] = await convertGeoLocation(city);
+            const hourlyInfo = await fetchWeatherInfo(latitude, longitude);
+
+            const NUMBER_OF_HOURS = 4;
+            const temperature_info = [];
+            for (let hour_idx = 0; hour_idx < NUMBER_OF_HOURS; ++hour_idx) {
+                temperature_info.push(hourlyInfo[hour_idx]["temp"]);
+            }
+            return temperature_info;
+        }
+
+        fetchHourlyWeather(city).then(
+            (result) => {
+                setIsLoaded(true);
+                setHourlyData([...result]);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
     }, [city]);
 
     if (error) {
