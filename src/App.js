3722ec68564eac4782a057f369687924a58ae3de
yyjlincoln <yyjlincoln@gmail.com>
2022-01-20 02:19:41 +1100

    feat: Shows no result instead of loading when the api returns 400; Gets the current location and show it upon opening the page

--- src/App.js
+++ src/App.js
@@ -5,26 +5,52 @@ import logo from './mlh-prep.png'
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City")
+  const [city, setCity] = useState(null)
   const [results, setResults] = useState(null);
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_APIKEY}`)
+          .then(res => res.json())
+          .then(
+            (result) => {
+              setIsLoaded(true);
+              setResults(result);
+              setCity(`${result.name}, ${result.sys.country}`);
+            }
+          )
+          .catch(
+            (error) => {
+              setIsLoaded(true);
+              setError(error);
+            }
+          )
+      }, () => {
+        setCity("");
+      })
+  }, [])
+
+  useEffect(() => {
+    setResults(null);
+    setIsLoaded(false);
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
       .then(res => res.json())
       .then(
         (result) => {
-          if (result['cod'] !== 200) {
-            setIsLoaded(false)
-          } else {
-            setIsLoaded(true);
+          if (result['cod'] === 200) {
             setResults(result);
+          } else {
+            setResults(null);
           }
         },
         (error) => {
-          setIsLoaded(true);
           setError(error);
         }
-      )
+      ).finally(() => {
+        setIsLoaded(true);
+      })
   }, [city])
 
   if (error) {
@@ -36,16 +62,18 @@ function App() {
         <h2>Enter a city below 👇</h2>
         <input
           type="text"
-          value={city}
+          value={city ?? "Loading Your Location..."}
           onChange={event => setCity(event.target.value)} />
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
-          {console.log(results)}
           {isLoaded && results && <>
             <h3>{results.weather[0].main}</h3>
             <p>Feels like {results.main.feels_like}°C</p>
             <i><p>{results.name}, {results.sys.country}</p></i>
           </>}
+          {
+            isLoaded && !results && <h2>No Results Found</h2>
+          }
         </div>
       </div>
     </>
