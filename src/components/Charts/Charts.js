b8b36b23aad422f2e670b8f2f2a8586dfa8a954e
Kshitij Darekar <kshitijdarekar13@gmail.com>
2022-01-27 00:41:32 +0530

    add line chart

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -4,17 +4,21 @@ import {
   CategoryScale,
   LinearScale,
   BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 import useIsMounted from '../../hooks/useIsMounted';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
   BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
@@ -55,54 +59,74 @@ export const options = {
 function Charts() {
   const [chartData, setChartData] = useState({});
   const isMounted = useIsMounted();
-  useEffect(() => {
-    if (isMounted.current) {
-      const fetchPrices = async () => {
-        try {
-          const res = await fetch(
-            'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=150ced7b197627bc73e83666c6875778'
-          ); // https://api.coincap.io/v2/assets/?limit=5
-          const data = await res.json();
-          console.log(data);
-          setChartData({
-            labels: ['temp', 'pressure', 'humidity'],
-            datasets: [
-              {
-                label: 'Pressure',
-                data: data.daily.map(({ pressure }) => pressure),
-                backgroundColor: [
-                  '#ffbb11',
-                  '#ecf0f1',
-                  '#50AF95',
-                  '#f3ba2f',
-                  '#2a71d0',
-                ],
-              },
-              {
-                label: 'Humidity',
-                data: data.daily.map(({ humidity }) => humidity),
-                backgroundColor: [
-                  '#ffbb11',
-                  '#ecf0f1',
-                  '#50AF95',
-                  '#f3ba2f',
-                  '#2a71d0',
-                ],
-              },
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid=150ced7b197627bc73e83666c6875778'
+      ); // https://api.coincap.io/v2/assets/?limit=5
+      const data = await res.json();
+      console.log("console 1 =",data);
+      setChartData({
+        labels: data.daily.map(({dt})=> new Date(dt).toLocaleString()),
+        datasets: [
+          {
+            label: 'Pressure',
+            data: data.daily.map(({ pressure }) => pressure),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: [
+              // '#ffbb11',
+              '#ecf0f1',
+              // '#50AF95',
+              // '#f3ba2f',
+              // '#2a71d0',
             ],
-          });
-        } catch (e) {
-          console.log(e);
-        }
-      };
-      fetchPrices();
+          },
+          {
+            label: 'Humidity',
+            data: data.daily.map(({ humidity }) => humidity),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: [
+            //   '#ffbb11',
+            //   '#ecf0f1',
+              '#50AF95',
+              // '#f3ba2f',
+            //   '#2a71d0',
+            ],
+          },
+          {
+            label: 'Temperature',
+            data: data.daily.map(({ humidity }) => humidity),
+            borderColor: 'rgb(53, 262, 135)',
+            backgroundColor: [
+              '#ffbb11',
+              // '#ecf0f1',
+              // '#50AF95',
+              // '#f3ba2f',
+              // '#2a7100',
+            ],
+          },
+        ],
+      });
+    } catch (e) {
+      console.log('==> ', e);
     }
-  }, [isMounted]);
-  console.log(chartData);
+  };
+
+  useEffect(() => {
+    // if (isMounted.current) {
+    fetchData();
+    // }
+  }, []);
+  console.log("console 3=",chartData);
   return (
-    <div>
-      <h1>{chartData.labels[1]}</h1>
-      <Bar data={chartData} />
+    <div backgroundColor='yellow'>
+      { chartData && <>
+      <h1>{chartData.labels[1]}</h1> 
+      <Bar data={chartData} options={{responsive:true}}/>
+      <Line data={chartData} options={{responsive:true}} />
+      </>}
+      <h1>Chart</h1>
+
     </div>
   );
 }
