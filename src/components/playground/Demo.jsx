580f57b6a84542675b396efcc9ad07270e8950a0
ditsuke <ditsuke@protonmail.com>
2022-01-27 04:48:50 +0530

    Add a header and move element definitions

--- src/components/playground/Demo.jsx
+++ src/components/playground/Demo.jsx
@@ -1,74 +1,26 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { Container } from '@mui/material';
 
+import logo from '../../mlh-prep.png';
+import {
+  ForecastWrapper,
+  Main,
+  MapWrapper,
+  SearchBarWrapper, SuggestionsWrapper,
+  WeatherAndMapContainer, WeatherCurrentWrapper,
+  WeatherWarningsWrapper,
+} from './Elements';
+
 function Demo() {
-  const Main = styled.main`
-    /** @todo: Get rid of the min height later */
-    min-height: 100vh;
-    display: grid;
-    grid-auto-rows: max-content;
-    gap: 2% 2%;
-    grid-auto-flow: row;
-
-    & * {
-      border: 1px solid red;
-      position: relative;
-    }
-
-    /* @remove Content placeholders -- remove once we have things in place */
-    & *:after {
-      content: attr(class);
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      display: grid;
-      align-items: center;
-      justify-content: center;
-    }
-  `;
-  const SearchBarWrapper = styled.div`
-    //grid-area: search-bar;
-    height: 2em;
-  `;
-  const WeatherAndMapContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-  `;
-  const WeatherCurrentWrapper = styled.div`
-    //grid-area: current-weather;
-    height: 469px;
-    width: 100%;
-    min-width: 300px;
-    flex: 2;
-  `;
-  const MapWrapper = styled.div`
-    //grid-area: map;
-    height: 469px;
-    width: 100%;
-    min-width: 300px;
-    flex: 1;
-  `;
-  const WeatherWarningsWrapper = styled.div`
-    //grid-area: weather-warnings;
-    /**@todo remove this static height when placing the component */
-    height: 400px;
-  `;
-  const ForecastWrapper = styled.div`
-    //grid-area: weather-forecast;
-    height: 400px;
-  `;
-  const SuggestionsWrapper = styled.div`
-    //grid-area: weather-suggestions;
-    height: 371px;
-  `;
-
   /* eslint-disable -- @todo get rid of this later */
   return (
     <>
+      <header style={{
+      //  For now... @todo remove
+        marginBottom: '2em',
+      }}>
+        <img src={logo} alt='' className='logo' />
+      </header>
       <Container maxWidth={'md'}>
         <Main>
           <SearchBarWrapper>
