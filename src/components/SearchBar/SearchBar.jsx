f3e91650522418d9176b61a337b7887cbd860548
ditsuke <ditsuke@protonmail.com>
2022-01-28 01:44:58 +0530

    Add refactored location search and pinning

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,65 @@
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+import { MyLocationRounded } from '@mui/icons-material';
+import { useState } from 'react';
+import './SearchBar.css';
+import cities from './assets/cities.json';
+
+// We need this transformation because ReactSearchAutocomplete only accepts object lists
+const cityList = (() => {
+  const objectList = [];
+  cities.forEach((city) => {
+    objectList.push({ n: city });
+  });
+
+  return objectList;
+})();
+
+export default function SearchBar({ setCity, autoFocus = true }) {
+  const [currentSearch, setSearch] = useState(null);
+
+  return (
+    <div id="search-and-location-pin-container">
+      <div id="search-bar-container">
+        <ReactSearchAutocomplete
+          items={[
+            {
+              n: currentSearch,
+            },
+            ...cityList,
+          ]}
+          fuseOptions={{
+            keys: ['n'],
+          }}
+          resultStringKeyName="n"
+          onSelect={(selectedCity) => setCity(selectedCity.n)}
+          onSearch={(search) => setSearch(search)}
+          styling={{
+            borderRadius: '5px',
+          }}
+          autoFocus={autoFocus}
+          inputDebounce={400}
+          // inputSearchString={city ?? 'Loading Your Location...'}
+        />
+      </div>
+      <MyLocationRounded
+        className="fetchLocationBtn"
+        font="inherit"
+        style={{
+          fontSize: '2em',
+        }}
+        onClick={() => {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const { lat, lon } = position.coords;
+            /**
+             * ! Need the logic to accommodate this behavior, but it should
+             * work perfectly since {@see OpenWeatherMap::getData()} supports
+             * {lat, lon}.
+             */
+            setCity({ lat, lon });
+          });
+        }}
+      />
+      ;
+    </div>
+  );
+}
