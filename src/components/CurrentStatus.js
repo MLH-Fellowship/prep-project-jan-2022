23d6803ee96eb1d9cf53106e8789769c07157ff2
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-01-27 19:07:32 +0530

    update weatherIcons based on location

--- src/components/CurrentStatus.js
+++ src/components/CurrentStatus.js
@@ -5,13 +5,72 @@ import rain from '../assets/images/cs-bg.png';
 export default function CurrentStatus({
   temp,
   feelsLike,
-  iconClass = 'wi-day-sunny',
-  background = rain,
   weatherStatus,
   visibility,
   windSpeed,
   humidity,
 }) {
+
+  // To change background image and weather symbol based on the weather type
+  let background = rain;
+  let iconClass = 'wi-night-clear';
+
+  if (weatherStatus === 'Clouds'){  
+    background = rain
+    iconClass = 'wi-cloudy'
+  }
+  else if (weatherStatus === 'Clear'){
+    background = rain
+    iconClass = 'wi-cloud'
+  }
+  else if (weatherStatus === 'Dust'){
+    background = rain
+    iconClass = 'wi-dust'
+  }
+  else if (weatherStatus === 'Fog'){
+    background = rain
+    iconClass = 'wi-fog'
+  }
+  else if (weatherStatus === 'Haze'){
+    background = rain
+    iconClass = 'wi-haze'
+  }
+  else if (weatherStatus === 'Mist'){
+    background = rain
+    iconClass = 'wi-windy'
+  }
+  else if (weatherStatus === 'Rain'){
+    background = rain
+    iconClass = 'wi-rain'
+  }
+  else if (weatherStatus === 'Shower'){
+    background = rain
+    iconClass = 'wi-showers'
+  }
+  else if (weatherStatus === 'Smoke'){
+    background = rain
+    iconClass = 'wi-smoke'
+  }
+  else if (weatherStatus === 'Snow'){
+    background = rain
+    iconClass = 'wi-snow'
+  }
+  else if (weatherStatus === 'Squall'){
+    background = rain
+    iconClass = 'wi-strong-wind'
+  }
+  else if (weatherStatus === 'Thunderstorm'){
+    background = rain
+    iconClass = 'wi-thunderstorm'
+  }
+  else if (weatherStatus === 'Tornado'){
+      background = rain
+      iconClass = 'wi-tornado'
+  }
+
+
+  
+      
   return (
     <div className="cs-container">
       <img className="cs-bg" src={background} alt="weather status background" />
