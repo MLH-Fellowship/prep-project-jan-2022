daf78703826580f9b9eaef20c8e27b82fa9e879a
CIPHERTron <pritish.samal918@gmail.com>
2022-02-03 15:37:15 +0530

    make suggestions section dynamic

--- src/components/WeatherSuggestions/Suggestions.jsx
+++ src/components/WeatherSuggestions/Suggestions.jsx
@@ -1,25 +1,24 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-restricted-syntax */
 import React, { useEffect, useState } from 'react';
-import _ from 'lodash';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './Suggestions.css';
-import weatherItems from './constants/Items';
+import data from './constants/Items';
 import LottieControl from './Lottie';
 import facebook from '../../assets/images/fb.svg';
 import twitter from '../../assets/images/twitter.svg';
 
-export default function WeatherSuggestions(results) {
-  const [items, setItems] = useState([]);
-  const { results: result = {} } = results;
-  const { weather = [] } = result;
+export default function WeatherSuggestions({ results }) {
+  const [items, setItems] = useState({});
 
-  // Sets default value to 'Thunderstorm'
-  const [firstWeather = { main: 'Thunderstorm' }] = weather;
-  const weatherName = firstWeather.main;
   useEffect(() => {
-    // Set items according to the weather
-    setItems(weatherItems[weatherName]);
-  }, [weatherName]);
+    for (let i = 0; i < data.length; i += 1) {
+      if (data[i].type === results.weather[0].main.toLowerCase())
+        setItems(data[i]);
+    }
+  }, [results, items]);
+
   return (
     <div className="suggestions-container">
       <Carousel
@@ -30,21 +29,19 @@ export default function WeatherSuggestions(results) {
         showStatus={false}
         className="carousel"
       >
-        {/* Traverse through the items map */}
-        {Object.keys(items).length > 0 &&
-          Object.keys(items).map((item) => (
-            <div key={item} className="required-item">
-              <LottieControl animationData={items[item]} />
-            </div>
-          ))}
+        {items.animations.map((item, idx) => (
+          <div key={idx} className="required-item">
+            <LottieControl animationData={item} />
+          </div>
+        ))}
       </Carousel>
+
       <div className="suggestions-list-container">
         <div className="suggestions-list">
           <h2>You might want to bring:</h2>
-          {Object.keys(items).length > 0 &&
-            Object.keys(items).map((item) => (
-              <p key={item}>{_.startCase(item)}</p>
-            ))}
+          {items.weatherItems.map((item) => (
+            <p key={item}>{item}</p>
+          ))}
         </div>
         <div className="share-container">
           <h2 className="share-heading">Share on</h2>
@@ -59,7 +56,7 @@ export default function WeatherSuggestions(results) {
             </a>
             <a
               className="twitter-share-button"
-              href={`https://twitter.com/intent/tweet?text=${weatherName}`}
+              href="https://twitter.com/intent/tweet?text=clouds"
               target="_blank"
               rel="noreferrer"
             >
